b817114c45d41d725be9c62b87afd85587be02a3
shunmuga-pavithra <127046763+shunmuga-pavithra@users.noreply.github.com>
2023-03-12 01:16:41 +0530

    Add files via upload

---
+++ Launchfb.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launchfb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ChromeDriver driver = new ChromeDriver();
+driver.get("https://www.facebook.com");
+driver.manage().window().maximize();
+driver.close();
+	}
+
+}
